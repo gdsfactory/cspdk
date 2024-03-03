a0416f800acf441c9c4299a7694a394b6fbae02f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-03-03 07:55:22 -0800

    add grating layer

--- cspdk/cells.py
+++ cspdk/cells.py
@@ -11,9 +11,8 @@ from cspdk.tech import xs_rc, xs_ro, xs_sc, xs_so
 _mmi1x2 = partial(gf.components.mmi1x2, width_mmi=6, length_taper=20, width_taper=1.5)
 _mmi2x2 = partial(gf.components.mmi2x2, width_mmi=6, length_taper=20, width_taper=1.5)
 ################
-# rib cband
+# MMIs rib cband
 ################
-
 mmi1x2_rc = partial(
     _mmi1x2,
     length_mmi=32.7,
@@ -27,9 +26,8 @@ mmi2x2_rc = partial(
     cross_section=xs_rc,
 )
 ################
-# strip cband
+# MMIs strip cband
 ################
-
 mmi1x2_sc = partial(
     _mmi1x2,
     length_mmi=31.8,
@@ -42,9 +40,8 @@ mmi2x2_sc = partial(
     gap_mmi=0.5,
     cross_section=xs_sc,
 )
-
 ################
-# rib oband
+# MMIs rib oband
 ################
 mmi1x2_ro = partial(
     _mmi1x2,
@@ -59,9 +56,8 @@ mmi2x2_ro = partial(
     cross_section=xs_ro,
 )
 ################
-# strip oband
+# MMIs strip oband
 ################
-
 mmi1x2_so = partial(
     _mmi1x2,
     length_mmi=40.1,
@@ -75,11 +71,42 @@ mmi2x2_so = partial(
     cross_section=xs_so,
 )
 
+##############################
+# grating couplers Rectangular
+##############################
+
+_gc_rectangular = partial(
+    gf.components.grating_coupler_rectangular,
+    fill_factor=0.5,
+    layer_slab=None,
+    length_taper=350,
+)
+
+gc_rectangular_so = partial(
+    _gc_rectangular,
+    period=0.5,
+    cross_section=xs_so,
+)
+gc_rectangular_ro = partial(
+    _gc_rectangular,
+    period=0.5,
+    cross_section=xs_ro,
+)
+
+gc_rectangular_sc = partial(
+    _gc_rectangular,
+    period=0.63,
+    cross_section=xs_sc,
+)
+gc_rectangular_rc = partial(
+    _gc_rectangular,
+    period=0.5,
+    cross_section=xs_rc,
+)
 
 ################
-# Imported from PDK
+# Imported from Cornerstone MPW SOI 220nm GDSII Template
 ################
-
 gdsdir = PATH.gds
 import_gds = partial(gf.import_gds, gdsdir=gdsdir)
 
@@ -88,24 +115,6 @@ import_gds = partial(gf.import_gds, gdsdir=gdsdir)
 def CORNERSTONE_MPW_SOI_220nm_GDSII_Template() -> gf.Component:
     """Returns CORNERSTONE_MPW_SOI_220nm_GDSII_Template fixed cell."""
     return import_gds("CORNERSTONE MPW SOI 220nm GDSII Template.gds")
-
-
-@gf.cell
-def Cell0_SOI220_Full_1550nm_Packaging_Template() -> gf.Component:
-    """Returns Cell0_SOI220_Full_1550nm_Packaging_Template fixed cell."""
-    return import_gds("Cell0_SOI220_Full_1550nm_Packaging_Template.gds")
-
-
-@gf.cell
-def Cell0_SOI_Full_Institution_Name() -> gf.Component:
-    """Returns Cell0_SOI_Full_Institution_Name fixed cell."""
-    return import_gds("Cell0_SOI_Full_Institution_Name.gds")
-
-
-@gf.cell
-def Cell0_SOI_Half_Institution_Name() -> gf.Component:
-    """Returns Cell0_SOI_Half_Institution_Name fixed cell."""
-    return import_gds("Cell0_SOI_Half_Institution_Name.gds")
 
 
 @gf.cell
@@ -121,111 +130,9 @@ def Heater() -> gf.Component:
 
 
 @gf.cell
-def Layer_Designations() -> gf.Component:
-    """Returns Layer_Designations fixed cell."""
-    return import_gds("Layer_Designations.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_RIB_2x1_MMI() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_RIB_2x1_MMI fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_RIB_2x1_MMI.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_RIB_2x2_MMI() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_RIB_2x2_MMI fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_RIB_2x2_MMI.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_RIB_90_Degree_Bend() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_RIB_90_Degree_Bend fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_RIB_90_Degree_Bend.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_RIB_Grating_Coupler() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_RIB_Grating_Coupler fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_RIB_Grating_Coupler.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_RIB_Waveguide() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_RIB_Waveguide fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_RIB_Waveguide.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_RIB_Waveguide_Crossing() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_RIB_Waveguide_Crossing fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_RIB_Waveguide_Crossing.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_STRIP_2x1_MMI() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_STRIP_2x1_MMI fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_STRIP_2x1_MMI.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_STRIP_2x2_MMI() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_STRIP_2x2_MMI fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_STRIP_2x2_MMI.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_STRIP_90_Degree_Bend() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_STRIP_90_Degree_Bend fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_STRIP_90_Degree_Bend.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_STRIP_Grating_Coupler() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_STRIP_Grating_Coupler fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_STRIP_Grating_Coupler.gds")
-
-
-@gf.cell
-def SOI220nm_1310nm_TE_STRIP_Waveguide() -> gf.Component:
-    """Returns SOI220nm_1310nm_TE_STRIP_Waveguide fixed cell."""
-    return import_gds("SOI220nm_1310nm_TE_STRIP_Waveguide.gds")
-
-
-@gf.cell
 def SOI220nm_1310nm_TE_STRIP_Waveguide_Crossing() -> gf.Component:
     """Returns SOI220nm_1310nm_TE_STRIP_Waveguide_Crossing fixed cell."""
     return import_gds("SOI220nm_1310nm_TE_STRIP_Waveguide_Crossing.gds")
-
-
-@gf.cell
-def SOI220nm_1550nm_TE_RIB_2x1_MMI() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_RIB_2x1_MMI fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_RIB_2x1_MMI.gds")
-
-
-@gf.cell
-def SOI220nm_1550nm_TE_RIB_2x2_MMI() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_RIB_2x2_MMI fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_RIB_2x2_MMI.gds")
-
-
-@gf.cell
-def SOI220nm_1550nm_TE_RIB_90_Degree_Bend() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_RIB_90_Degree_Bend fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_RIB_90_Degree_Bend.gds")
-
-
-@gf.cell
-def SOI220nm_1550nm_TE_RIB_Grating_Coupler() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_RIB_Grating_Coupler fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_RIB_Grating_Coupler.gds")
-
-
-@gf.cell
-def SOI220nm_1550nm_TE_RIB_Waveguide() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_RIB_Waveguide fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_RIB_Waveguide.gds")
 
 
 @gf.cell
@@ -235,40 +142,16 @@ def SOI220nm_1550nm_TE_RIB_Waveguide_Crossing() -> gf.Component:
 
 
 @gf.cell
-def SOI220nm_1550nm_TE_STRIP_2x1_MMI() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_STRIP_2x1_MMI fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_STRIP_2x1_MMI.gds")
-
-
-@gf.cell
-def SOI220nm_1550nm_TE_STRIP_2x2_MMI() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_STRIP_2x2_MMI fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_STRIP_2x2_MMI.gds")
-
-
-@gf.cell
-def SOI220nm_1550nm_TE_STRIP_90_Degree_Bend() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_STRIP_90_Degree_Bend fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_STRIP_90_Degree_Bend.gds")
-
-
-@gf.cell
-def SOI220nm_1550nm_TE_STRIP_Waveguide() -> gf.Component:
-    """Returns SOI220nm_1550nm_TE_STRIP_Waveguide fixed cell."""
-    return import_gds("SOI220nm_1550nm_TE_STRIP_Waveguide.gds")
-
-
-@gf.cell
 def SOI220nm_1550nm_TE_STRIP_Waveguide_Crossing() -> gf.Component:
     """Returns SOI220nm_1550nm_TE_STRIP_Waveguide_Crossing fixed cell."""
     return import_gds("SOI220nm_1550nm_TE_STRIP_Waveguide_Crossing.gds")
 
 
 if __name__ == "__main__":
-    c = gf.Component()
-    run = c << mmi1x2_rc()
-    ref = c << SOI220nm_1550nm_TE_RIB_2x1_MMI()
-    ref.mirror()
-    ref.center = (0, 0)
-    run.center = (0, 0)
+    # ref = c << SOI220nm_1550nm_TE_RIB_2x1_MMI()
+    # ref.mirror()
+    # ref.center = (0, 0)
+    # run.center = (0, 0)
+    # c = gc_rectangular_ro()
+    c = CORNERSTONE_MPW_SOI_220nm_GDSII_Template()
     c.show()
