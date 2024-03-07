f9e74b41060da5446be6fe1b1b3317a77ef3bb6d
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-03-06 17:23:04 -0800

    fix circuit_simulations

--- cspdk/samples/circuit_simulations.py
+++ cspdk/samples/circuit_simulations.py
@@ -1,21 +1,21 @@
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
 import sax
 
 import cspdk
 from cspdk.models import models
 
 if __name__ == "__main__":
-    c = cspdk.cells.mzi_sc()
+    c = cspdk.cells.mzi_sc(delta_length=20)
     netlist = c.get_netlist()
     circuit, _ = sax.circuit(netlist, models=models)
-    # result = circuit()
+    wl = jnp.linspace(1.5, 1.6)
 
-    # wl = np.linspace(1.5, 1.6)
-    # S = circuit(wl=wl)
-
-    # plt.figure(figsize=(14, 4))
-    # plt.title("MZI")
-    # plt.plot(1e3 * wl, jnp.abs(S["o1", "o2"]) ** 2)
-    # plt.xlabel("λ [nm]")
-    # plt.ylabel("T")
-    # plt.grid(True)
-    # plt.show()
+    S = circuit(wl=wl)
+    plt.figure(figsize=(14, 4))
+    plt.title("MZI")
+    plt.plot(1e3 * wl, jnp.abs(S["o1", "o2"]) ** 2)
+    plt.xlabel("λ [nm]")
+    plt.ylabel("T")
+    plt.grid(True)
+    plt.show()
