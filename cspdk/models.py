e516090bc47e62fa0923b0eda713a0d13e817105
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-04-30 07:45:54 -0700

    update gplugins

--- cspdk/models.py
+++ cspdk/models.py
@@ -4,13 +4,12 @@ import inspect
 from collections.abc import Callable
 from functools import partial
 
-import jax
+import gplugins.sax.models as sm
 import jax.numpy as jnp
 import sax
 from gdsfactory.pdk import get_cross_section_name
 from gdsfactory.typings import CrossSectionSpec
 from gplugins.sax.models import bend as __bend
-from gplugins.sax.models import grating_coupler
 from gplugins.sax.models import straight as __straight
 from numpy.typing import NDArray
 
@@ -97,7 +96,6 @@ straight_no = partial(_straight, cross_section="xs_no")
 ################
 # Bends
 ################
-
 bend_s = _straight
 
 
@@ -129,54 +127,8 @@ trans_sc_rc50 = partial(_taper_cross_section, length=50.0)
 ################
 # MMIs
 ################
-
-
-def _mmi_amp(
-    wl: Float = 1.55, wl0: Float = 1.55, fwhm: Float = 0.2, loss_dB: Float = 0.3
-):
-    max_power = 10 ** (-abs(loss_dB) / 10)
-    f = 1 / wl
-    f0 = 1 / wl0
-    f1 = 1 / (wl0 + fwhm / 2)
-    f2 = 1 / (wl0 - fwhm / 2)
-    _fwhm = f2 - f1
-
-    sigma = _fwhm / (2 * jnp.sqrt(2 * jnp.log(2)))
-    power = jnp.exp(-((f - f0) ** 2) / (2 * sigma**2))
-    power = max_power * power / power.max() / 2
-    return jnp.sqrt(power)
-
-
-def _mmi1x2(
-    wl: Float = 1.55, wl0: Float = 1.55, fwhm: Float = 0.2, loss_dB: Float = 0.3
-) -> sax.SDict:
-    thru = _mmi_amp(wl=wl, wl0=wl0, fwhm=fwhm, loss_dB=loss_dB)
-    return sax.reciprocal(
-        {
-            ("o1", "o2"): thru,
-            ("o1", "o3"): thru,
-        }
-    )
-
-
-def _mmi2x2(
-    wl: Float = 1.55,
-    wl0: Float = 1.55,
-    fwhm: Float = 0.2,
-    loss_dB: Float = 0.3,
-    shift: Float = 0.005,
-) -> sax.SDict:
-    thru = _mmi_amp(wl=wl, wl0=wl0, fwhm=fwhm, loss_dB=loss_dB)
-    cross = 1j * _mmi_amp(wl=wl, wl0=wl0 + shift, fwhm=fwhm, loss_dB=loss_dB)
-    return sax.reciprocal(
-        {
-            ("o1", "o3"): thru,
-            ("o1", "o4"): cross,
-            ("o2", "o3"): cross,
-            ("o2", "o4"): thru,
-        }
-    )
-
+_mmi1x2 = sm.mmi1x2
+_mmi2x2 = sm.mmi2x2
 
 _mmi1x2_o = partial(_mmi1x2, wl0=1.31)
 _mmi1x2_c = partial(_mmi1x2, wl0=1.55)
@@ -214,27 +166,7 @@ coupler_no = _coupler_o
 ##############################
 # grating couplers Rectangular
 ##############################
-
-
-def _gc_rectangular(
-    *,
-    wl: Float = 1.55,
-    reflection: Float = 0.0,
-    reflection_fiber: Float = 0.0,
-    loss=0.0,
-    wavelength=1.55,
-    bandwidth: Float = 40 * nm,
-) -> sax.SDict:
-    return grating_coupler(
-        wl=wl,  # type: ignore
-        wl0=wavelength,
-        reflection=reflection,  # type: ignore
-        reflection_fiber=reflection_fiber,  # type: ignore
-        loss=loss,
-        bandwidth=bandwidth,  # type: ignore
-    )
-
-
+_gc_rectangular = sm.grating_coupler
 _gcro = partial(_gc_rectangular, loss=6, bandwidth=35 * nm, wavelength=1.31)
 _gcrc = partial(_gc_rectangular, loss=6, bandwidth=35 * nm, wavelength=1.55)
 gc_rectangular_sc = _gcrc
@@ -247,7 +179,6 @@ gc_rectangular_no = _gcro
 ##############################
 # grating couplers Elliptical
 ##############################
-
 _gc_elliptical = _gc_rectangular
 _gceo = partial(_gc_elliptical, loss=6, bandwidth=35 * nm, wavelength=1.31)
 _gcec = partial(_gc_elliptical, loss=6, bandwidth=35 * nm, wavelength=1.55)
@@ -262,19 +193,7 @@ gc_elliptical_no = _gceo
 ################
 # Crossings
 ################
-
-
-@jax.jit
-def _crossing(wl: Float = 1.5) -> sax.SDict:
-    one = jnp.ones_like(jnp.asarray(wl))
-    return sax.reciprocal(
-        {
-            ("o1", "o3"): one,
-            ("o2", "o4"): one,
-        }
-    )
-
-
+_crossing = sm.crossing
 crossing_so = _crossing
 crossing_rc = _crossing
 crossing_sc = _crossing
