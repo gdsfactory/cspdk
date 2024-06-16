be24a6cecbdbd768b41aaa5e2688bf6cd3bf04c3
flaport <floris.laporte@gmail.com>
2024-06-16 11:12:25 +0200

    update tests

--- tests/test_netlists_si220.py
+++ tests/test_netlists_si220.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from functools import partial
+
 import gdsfactory as gf
 import jsondiff
 import pytest
@@ -19,6 +21,50 @@ cells = PDK.cells
 skip_test = set()
 cell_names = cells.keys() - skip_test
 cell_names = [name for name in cell_names if not name.startswith("_")]
+
+
+def get_minimal_netlist(comp: gf.Component):
+    net = comp.get_netlist()
+
+    def _get_instance(inst):
+        return {
+            "component": inst["component"],
+            "settings": inst["settings"],
+        }
+
+    return {"instances": {i: _get_instance(c) for i, c in net["instances"].items()}}
+
+
+@pytest.mark.parametrize("name", cells)
+def test_cell_in_pdk(name):
+    c1 = gf.Component()
+    c1.add_ref(gf.get_component(name))
+    net1 = get_minimal_netlist(c1)
+
+    c2 = gf.read.from_yaml(net1)
+    net2 = get_minimal_netlist(c2)
+
+    return net1["instances"] == net2["instances"]
+
+
+@pytest.mark.parametrize("name", cells)
+def test_partial_of_base_cell(name):
+    pdk = gf.get_active_pdk()
+    func = pdk.cells[name]
+    if not isinstance(func, partial):
+        if hasattr(func, "_base_cell"):
+            return
+        elif getattr(getattr(func, "__wrapped__", None), "__module__", "").startswith(
+            "cspdk"
+        ):
+            return
+        else:
+            raise ValueError(f"Cell {name!r} does not belong to pdk.")
+    while isinstance(func, partial):
+        func = func.func
+    assert getattr(
+        func, "_base_cell", False
+    ), f"{name} is not a partial of a base cell."
 
 
 @pytest.mark.parametrize("component_type", cell_names)
