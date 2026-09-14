c9ce0095bd0e1bbbd771117d2366a00d478ba6af
flaport <floris.laporte@gmail.com>
2026-09-14 21:25:11 +0200

    fix: expose electrical connectivity on runtime PDKs

--- cspdk/si340/tech.py
+++ cspdk/si340/tech.py
@@ -3,7 +3,6 @@
 import sys
 from collections.abc import Iterable
 from functools import partial
-from typing import cast
 
 import gdsfactory as gf
 from gdsfactory.cross_section import (
@@ -56,6 +55,8 @@ class LayerMapCornerstone(LayerMap):
 
 
 LAYER = LayerMapCornerstone
+
+CONNECTIVITY: list[ConnectivitySpec] = [("HEATER", "HEATER", "PAD")]
 
 
 def get_layer_stack(
@@ -384,13 +385,11 @@ if __name__ == "__main__":
 
     LAYER_VIEWS = LayerViews(PATH.lyp_yaml)
 
-    connectivity = cast(list[ConnectivitySpec], [("HEATER", "HEATER", "PAD")])
-
     t = KLayoutTechnology(
         name="Cornerstone_si340",
         layer_map=LAYER,
         layer_views=LAYER_VIEWS,
         layer_stack=LAYER_STACK,
-        connectivity=connectivity,
+        connectivity=CONNECTIVITY,
     )
     t.write_tech(tech_dir=PATH.klayout)
